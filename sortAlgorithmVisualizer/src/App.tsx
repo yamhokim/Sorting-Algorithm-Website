d2897081bb28708ff980c7b94e39bfb1f307a279
JPSahel <jpsahelsk@gmail.com>
2024-12-16 22:55:29 -0500

    Created layout for project in CSS

--- sortAlgorithmVisualizer/src/App.tsx
+++ sortAlgorithmVisualizer/src/App.tsx
@@ -1,21 +1,30 @@
-import ArrayBar from "./components/arrayBar"
-import Header from "./components/header"
-import "./index.css"
+import "./index.css";
 
 function App() {
-
-
   return (
     <>
-    <div>
-      <h1>Sort Algorithm Visualizer</h1>
-      <ArrayBar />
-      <Header />
-    </div>
-    <div>
-    </div>
+      <div className="container">
+        <div className="sidebar">
+          <div className="button-container">
+            <button>Generate New Array</button>
+            <button>Bubble Sort</button>
+            <button>Selection Sort</button>
+            <button>Insertion Sort</button>
+            <button>Quick Sort</button>
+            <button>Merge Sort</button>
+          </div>
+        </div>
+        <div className="main-content">
+          <div className="title"></div>
+          <div className="array-container">
+            <div className="array-bar"></div>
+          </div>
+          <div className="textbox"></div>
+          <div className="sliders"></div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
