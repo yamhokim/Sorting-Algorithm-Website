2418e2e523f2f4f3b52776671cc10ba355cf06c9
JPSahel <jpsahelsk@gmail.com>
2024-12-30 12:25:54 -0500

    arrayBar

--- sortAlgorithmVisualizer/src/components/arrayBar.tsx
+++ sortAlgorithmVisualizer/src/components/arrayBar.tsx
@@ -1,6 +1,22 @@
 import "../index.css";
+
 function arrayBar() {
-  return <div className="array-bar">Array Bar</div>;
+  //create an array of random numbers
+  const numarray = Array.from({ length: 10 }, () =>
+    Math.floor(Math.random() * 100)
+  );
+
+  return (
+    <div className="array-bar flex items-end justify-center h-full">
+      {numarray.map((heightnum, index) => (
+        <div
+          key={index}
+          className="w-4 border border-cyan-500 bg-gray-50"
+          style={{ height: `${heightnum}px` }}
+        ></div>
+      ))}
+    </div>
+  );
 }
 
 export default arrayBar;
--- sortAlgorithmVisualizer/src/components/sliders.tsx
+++ sortAlgorithmVisualizer/src/components/sliders.tsx
@@ -1,5 +1,10 @@
 import "../index.css";
 function sliders() {
-  return <div className="sliders">Slider</div>;
+  return (
+    <div className="sliders">
+      <div className="bg-black h-10 w-40 flex  absolute left-1/2"></div>
+      <div className="bg-black h-10 w-40 flex  absolute right-1/2"></div>
+    </div>
+  );
 }
 export default sliders;
