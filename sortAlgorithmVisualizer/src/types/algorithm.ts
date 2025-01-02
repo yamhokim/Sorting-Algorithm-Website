07176f8578bc7d48d321d4c487d17888f60da0d3
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-02 00:11:29 -0500

    created a separate directory for type declarations

---
+++ sortAlgorithmVisualizer/src/types/algorithm.ts
@@ -0,0 +1,7 @@
+export type SortingAlgorithm = (
+  numarray: number[],
+  setNumArray: React.Dispatch<React.SetStateAction<number[]>>,
+  setActiveIndices: React.Dispatch<React.SetStateAction<number[]>>,
+  setSwappedIndices: React.Dispatch<React.SetStateAction<number[]>>,
+  stepDuration: number
+) => void;
