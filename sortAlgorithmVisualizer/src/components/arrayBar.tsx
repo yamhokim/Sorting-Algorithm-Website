32355597034a2081849537c350f4cc76eb8b77c0
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-02 00:15:11 -0500

    commented out the swap index button for now

--- sortAlgorithmVisualizer/src/components/arrayBar.tsx
+++ sortAlgorithmVisualizer/src/components/arrayBar.tsx
@@ -99,12 +99,12 @@ function ArrayBar({ name, amountValue, speedValue, sortingAlgorithm }: ArrayBarP
       </div>
 
       <div className="mt-4 flex gap-2">
-        <button
+        {/* <button
           className="px-4 py-2 bg-blue-500 text-white rounded"
           onClick={() => handleSwap(0, 1)}
         >
           Swap Index 0 & 1
-        </button>
+        </button> */}
         <button
           className="px-4 py-2 bg-green-500 text-white rounded"
           onClick={() => sortingAlgorithm(numarray, setNumArray, setActiveIndices, setSwappedIndices, stepDuration)}
